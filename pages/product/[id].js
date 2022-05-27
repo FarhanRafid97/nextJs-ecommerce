669216cdf11502a3191ae568c8653dae0c0c5aa7
farhan <farhanrafid97@gmail.com>
2022-05-27 17:40:00 +0700

    add staticProps

---
+++ pages/product/[id].js
@@ -0,0 +1,49 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { detailPorduct } from '../../src/redux/actions/product';
+import detailStyle from './DetailProduct.module.css';
+
+const index = ({ params }) => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const product = useSelector((state) => state.product);
+
+  useEffect(() => {
+    dispatch(detailPorduct(params.id));
+  }, [dispatch, router]);
+  console.log(product);
+
+  return (
+    <div className={detailStyle.detailContainer}>
+      <div className={detailStyle.photoProduct}></div>
+      <div className={detailStyle.detailProductDesc}>
+        <h3>{product?.title}</h3>
+        <p>{product?.price}</p>
+        <p>{product?.description}</p>
+        <button onClick={() => router.back()}> BACK </button>
+      </div>
+    </div>
+  );
+};
+export function getStaticProps(context) {
+  return {
+    props: { params: context.params },
+  };
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://fakestoreapi.com/products`);
+
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default index;
