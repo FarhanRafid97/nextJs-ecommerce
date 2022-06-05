c6526c81bfece172cd73a2732589fdc0c1e59a46
farhan <farhanrafid97@gmail.com>
2022-06-05 19:03:12 +0700

    fix

--- components/home/HomeIndex.js
+++ components/home/HomeIndex.js
@@ -3,7 +3,6 @@ import product from '../../product.json';
 import NextLink from 'next/link';
 import { Flex, Image, Box, Text, Link } from '@chakra-ui/react';
 import homeStyle from './home.module.css';
-import { motion } from 'framer-motion';
 
 const HomeIndex = () => {
   const dataProductHome = product.products.filter(
